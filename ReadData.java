5556068a7d23b6a969bd5a59638209695eef8273
prethamrb07 <prethamrb07@gmail.com>
2025-10-16 10:21:53 -0700

    Add files via upload

---
+++ ReadData.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.io.*;
+public class ReadData {
+   public static void main(String [] args){
+      int totalSongs = 0;
+      int total1982 = 0;
+      int total2000 = 0;
+      int total2015 = 0;
+      Scanner in;
+      int count;
+      try{
+         in = new Scanner (new File("songs 2025-2026ANSI.csv"));
+         while(in.hasNext()){
+            count++;
+            System.out.println(count);
+            System.out.println(in.nextLine().split(";")[2]);
+            totalSongs++;
+            
+            String artist = in.nextLine().split(";")[0];
+            String track = in.nextLine().split(";")[1];
+            int release_date = Integer.parseInt(in.nextLine().split(";")[2]);
+            String genre = in.nextLine().split(";")[3];
+            int duration = Integer.parseInt(in.nextLine().split(";")[4]);
+            double shake = Integer.parseInt(in.nextLine().split(";")[5]);
+            String obcene = in.nextLine().split(";")[6];
+            String danceability = in.nextLine().split(";")[7];
+            String loudness = in.nextLine().split(";")[8];
+            String topic = in.nextLine().split(";")[9];
+
+            songs[count] = new Song(String artist, String track, int release_date, String genre, int duration, double shake, String obcene, String danceability, String loudness, String topic);
+         }  
+         in.close();
+      }catch(IOException e){
+         System.out.println("Error");
+   
+   }
+   System.out.println(totalSongs);
+   }
+}
---
+++ Song.java
@@ -0,0 +1,10 @@
+public class Song {
+   private String artist, title, genre;
+   private int year;
+   private double danceability;
+   
+   public class(String artist, String title, String genre, int year, double danceability)
+   {
+      this artist = artist;
+   }
+}
