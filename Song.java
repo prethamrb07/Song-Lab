dea40b22a5902b3ff90f8f05aadfdd42ca464d4d
prethamrb07 <prethamrb07@gmail.com>
2025-10-17 10:28:38 -0700

    Update Song.java

--- Song.java
+++ Song.java
@@ -3,8 +3,21 @@ public class Song {
    private int year;
    private double danceability;
    
-   public class(String artist, String title, String genre, int year, double danceability)
+   public Song(String artist, String title, String genre, int year, double danceability)
    {
-      this artist = artist;
+      this.artist = artist;
+      this.title = title;
+      this.genre = genre;
+      this.year = year;
+      this.danceability = danceability;
+   }
+   public String getArtist(){
+      return artist;
+   }
+   public String getTitle(){
+      return title;
+   }
+   public int getYear(){
+      return year;
    }
 }
